b90e466fe2c28f6b312422ade71c2c3c3303ab96
ricktronic <ricktronic@outloo.com>
2025-11-19 21:57:58 -0300

    entrega 3

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import { initRouter } from "./router.js";
-import { initFormValidation } from "./formValidation.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initRouter();
-  initFormValidation();
-});
